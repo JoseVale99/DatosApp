6a077713f544bc7e2c09d7e8113873758210c309
Zarate Carreno Jose Valentin <lc19160007@oaxaca.tecnm.mx>
2021-11-09 15:10:50 -0600

    ✅ Adding: new case in function

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -3,47 +3,60 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use \TRegx\SafeRegex\preg;
 
 class DataController extends Controller
 {
-//  Return the main view
+    //  Return the main view
     public function index()
     {
-       return view('index');
+        return view('index');
     }
 
-    
-    public function create()
-    {
-        //
-    }
 
-   
     public function store(Request $request)
     {
-        //
-    }
 
-   
-    public function show($id)
-    {
-        //
-    }
+        // Type of operations
 
-   
-    public function edit($id)
-    {
-        //
-    }
+        // $var = str_replace("/","|",$request->dato);
 
-  
-    public function update(Request $request, $id)
-    {
-        //
-    }
+        switch (true) {
+            case is_null($request->dato):
+                return back()->with('message_error', "¡Por favor ingrese un dato!");
+                break;
+            case preg_match("/^([0-9 .]+([+][0-9.]+))?$/", $request->dato);
+                return back()->with('message', "La siguiente operación $request->dato corresponde a una suma");
+                break;
+            case preg_match("/^([0-9 .]+([-][0-9.]+))?$/", $request->dato):
+                return back()->with('message', "La siguiente operación $request->dato corresponde a una resta");
+                break;
 
-    public function destroy($id)
-    {
-        //
+            case preg_match("/^([0-9 .]+([*][0-9.]+))?$/", $request->dato):
+                return back()->with('message', "La siguiente operación $request->dato corresponde a una multiplicación");
+                break;
+            case preg_match("/^([0-9 .]+([|][0-9.]+))?$/", $request->dato):
+                return back()->with('message', "La siguiente operación $request->dato corresponde a una división");
+                break;
+
+            case preg_match("/^(0|[1-9][0-9]*)$/", $request->dato):
+                return back()->with('message', "El dato $request->dato corresponde a un valor numerico de tipo entero (int)");
+                break;
+            case preg_match("/^[+-]?([0-9]+\.?[0-9]*|\.[0-9]+)$/", $request->dato):
+                return back()->with('message', "El dato $request->dato corresponde a un valor numerico de tipo decimal (float)");
+                break;
+            case $request->dato == "true" || $request->dato == "false":
+                return back()->with('message', "El dato $request->dato corresponde a un valor booleano de tipo (Boolean)");
+                break;
+            case is_string($request->dato):
+                return back()->with('message', "El dato $request->dato corresponde a una cadena de tipo (String)");
+                break;
+        }
+
+
+
+
+
+        return back();
     }
 }
