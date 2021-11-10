80fee3db2a9be67a632ff114f5dde45c999c6b09
Zarate Carreno Jose Valentin <lc19160007@oaxaca.tecnm.mx>
2021-11-10 03:24:25 -0600

    🐛 FIX: url index

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-lg">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/index') }}">
+                <a class="navbar-brand" href="{{ url('/') }}">
                     Tipos de datos
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
