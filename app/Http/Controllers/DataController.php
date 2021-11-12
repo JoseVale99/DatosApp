1f3eb70e86530949b6a43a241b6783e6ec2676d4
Zarate Carreno Jose Valentin <lc19160007@oaxaca.tecnm.mx>
2021-11-12 04:22:40 -0600

    🐛 FIX: desing and new components

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -20,36 +20,27 @@ class DataController extends Controller
         // Type of operations
 
         // $var = str_replace("/","|",$request->dato);
-
+       
         switch (true) {
+            
             case is_null($request->dato):
                 return back()->with('message_error', "¡Por favor ingrese un dato!");
                 break;
-            case preg_match("/^([0-9 .]+([+][0-9.]+))?$/", $request->dato);
-                return back()->with('message', "La siguiente operación $request->dato corresponde a una suma");
-                break;
-            case preg_match("/^([0-9 .]+([-][0-9.]+))?$/", $request->dato):
-                return back()->with('message', "La siguiente operación $request->dato corresponde a una resta");
-                break;
-
-            case preg_match("/^([0-9 .]+([*][0-9.]+))?$/", $request->dato):
-                return back()->with('message', "La siguiente operación $request->dato corresponde a una multiplicación");
-                break;
-            case preg_match("/^([0-9 .]+([|][0-9.]+))?$/", $request->dato):
-                return back()->with('message', "La siguiente operación $request->dato corresponde a una división");
-                break;
-
             case preg_match("/^(0|[-1-9][0-9]*)$/", $request->dato):
-                return back()->with('message', "El dato $request->dato corresponde a un valor numerico de tipo entero (int)");
+               
+                return back()->with('message',"int $request->dato");
                 break;
             case preg_match("/^([-0-9]+[.]([0-9]+))?$/", $request->dato):
-                return back()->with('message', "El dato $request->dato corresponde a un valor numerico de tipo decimal (float)");
+                return back()->with('message', "float $request->dato");
                 break;
             case $request->dato == "true" || $request->dato == "false":
-                return back()->with('message', "El dato $request->dato corresponde a un valor booleano de tipo (Boolean)");
+                return back()->with('message', "boolean $request->dato");
+                break;
+            case preg_match("/^([a-z A-Z @ # ~ € & · $ ( )  % = ? { } - + * |])$/", $request->dato):
+                return back()->with('message', "char $request->dato");
                 break;
             case is_string($request->dato):
-                return back()->with('message', "El dato $request->dato corresponde a una cadena de tipo (String)");
+                return back()->with('message', "str $request->dato");
                 break;
         }
 
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -36,22 +36,8 @@
                         <div class="alert alert-primary" role="alert">
                             <h4 class="alert-heading">¿Cómo se utiliza la App?</h4>
                             A continuacion te muestro los ejemplos sencillos de realizar.
-
-
                             <div class="container">
                                 <div class="row text-dark">
-                                    <div class="col font-weight-bold">Datos de entrada</div>
-                                    <div class="col font-weight-bold">Salida</div>
-                                    <div class="w-100"></div>
-                                    <div class="col">5+2</div>
-                                    <div class="col">operación suma</div>
-                                    <div class="w-100"></div>
-                                    <div class="col">5|2</div>
-                                    <div class="col">operación: resta</div>
-                                    <div class="w-100"></div>
-                                    <div class="col">5*2</div>
-                                    <div class="col">operación: multiplicación</div>
-                                    <div class="w-100"></div>
                                     <div class="col">5</div>
                                     <div class="col">número: entero (int)</div>
                                     <div class="w-100"></div>
@@ -66,6 +52,9 @@
                                     <div class="w-100"></div>
                                     <div class="col">false</div>
                                     <div class="col">Valor booleano (boolean)</div>
+                                    <div class="w-100"></div>
+                                    <div class="col">#</div>
+                                    <div class="col">Char</div>
 
                                 </div>
                             </div>
@@ -81,24 +70,176 @@
                                     {{ session('message_error') }}
                                 </div>
                             @endif
-                            <div class="form-group row">
-                                <label for="inputPassword" class="col-sm-2 col-form-label font-weight-bold">Datos de
+                            <div class="row justify-content-center">
+                                <label class="col-sm-2 col-form-label mt-3 font-weight-bold">Datos de
                                     entrada</label>
-                                <div class="col-sm-10">
+                                <div class="col-md-auto mt-3">
                                     <input type="text" name="dato" class="form-control"
-                                        placeholder="Ingrese un dato por ej. 3+2">
-                                </div>
-                            </div>
-                            @if (session('message'))
-                                <div class="alert alert-dark  font-weight-bold" role="alert">
-                                    {{ session('message') }}
-                                </div>
-                            @endif
+                                        placeholder="Ingrese un dato p. Ej., 1.234">
+                                        
+                                    </div>
 
-
-                            <div class="form-group row justify-content-center mt-3">
-                                <button type="submit" class="btn btn-outline-dark btn-lg">Verificar</button>
+                                    <div class="col-md-auto mt-3 ">
+                                        <button type="submit" class="btn btn-outline-dark btn-block">Verificar</button>
+                                    </div>
+                                    
+                                       
+                                    
+                                
                             </div>
+                            <br>
+                            <br>
+                            @if (session('message'))  
+                            <div class="alert alert-success rounded" role="alert">
+                            @if (str_contains(Session::get('message'),"int"))
+                                       <h5 class="font-weight-bold">El dato {{str_replace("int","",Session::get('message'))}} corresponde a un valor numerico de tipo entero (int) </h5>   
+                                       <br>
+                                       <h3 class="alert-heading">¿Qué podemos hacer con este tipo de dato?</h3>
+                                        <hr>
+                                        <h6>Se puede realizar las siguientes operaciones, por mencionar algunas:</h6>
+                                        <ul>
+                                            <li>Suma</li>
+                                            <li>Resta</li>
+                                            <li>Multiplicación</li>
+                                            <li>División</li>
+                                            <li>Potencia</li>
+                                            <li>Raiz cuadrada</li>
+                                        </ul>
+                                        <br>
+                                        <h4>Ejemplo en el lenguaje Python</h4>
+                                        <h1 class="text-center">
+                                            <img class="img-fluid" src="https://i.ibb.co/Tt02SMP/example-py.png" width="400">
+                                        </h1>
+                                    @elseif(str_contains(Session::get('message'),"float"))
+                                    <h5 class="font-weight-bold">El dato {{str_replace("float","",Session::get('message'))}} corresponde a un valor numerico de tipo decimal (float) </h5>   
+                                    <br>
+                                    <h3 class="alert-heading">¿Qué podemos hacer con este tipo de dato?</h3>
+                                     <hr>
+                                     <h6>Se puede realizar las siguientes operaciones, por mencionar algunas:</h6>
+                                     <ul>
+                                         <li>Suma</li>
+                                         <li>Resta</li>
+                                         <li>Multiplicación</li>
+                                         <li>División</li>
+                                         <li>Potencia</li>
+                                         <li>Raiz cuadrada</li>
+                                     </ul>
+                                     <br>
+                                     <h4>Ejemplo en el lenguaje Python</h4>
+                                     <h4 class="text-center"><img class="img-fluid" src="https://i.ibb.co/NSvmYV2/example-py.png" width="400"></h4>
+                                  
+                                     @elseif(str_contains(Session::get('message'),"boolean"))
+                                     <h5 class="font-weight-bold">El dato {{str_replace("boolean","",Session::get('message'))}} corresponde a un valor de tipo booleano (boolean) </h5>   
+                                     <br>
+                                     <h6>Un valor booleano representa un valor de verdad; es decir, TRUE o FALSE. Una expresión booleana o predicado puede producir un valor de desconocido (unknown), que está representado por el valor nulo.</h6>
+                                     <br>
+                                     <h3 class="alert-heading">¿Qué podemos hacer con este tipo de dato?</h3>
+                                      <hr>
+                                      <h6>Se puede realizar las siguientes operaciones, por mencionar algunas:</h6>
+                                      <ul>
+                                          <li>Expresiones booleanas</li>
+                                          <li>Comparar cadenas</li>
+                                          <li>Comparar numeros</li>
+                                          
+                                      </ul>
+                                      <br>
+                                      <h4>Ejemplo en el lenguaje Python</h4>
+                                      <h4 class="text-center"><img class="img-fluid" src="https://i.ibb.co/bsdFH76/example-py.png" width="400"></h4>
+                                     
+                          
+                            </div>
+                            @elseif(str_contains(Session::get('message'),"float"))
+                            <h5 class="font-weight-bold">El dato {{str_replace("float","",Session::get('message'))}} corresponde a un valor numerico de tipo decimal (float) </h5>   
+                            <br>
+                            <h3 class="alert-heading">¿Qué podemos hacer con este tipo de dato?</h3>
+                             <hr>
+                             <h6>Se puede realizar las siguientes operaciones, por mencionar algunas:</h6>
+                             <ul>
+                                 <li>Suma</li>
+                                 <li>Resta</li>
+                                 <li>Multiplicación</li>
+                                 <li>División</li>
+                                 <li>Potencia</li>
+                                 <li>Raiz cuadrada</li>
+                             </ul>
+                             <br>
+                             <h4>Ejemplo en el lenguaje Python</h4>
+                             <h4 class="text-center"><img class="img-fluid" src="https://i.ibb.co/NSvmYV2/example-py.png" width="400"></h4>
+                          
+                             @elseif(str_contains(Session::get('message'),"char"))
+                             <h5 class="font-weight-bold">El dato {{str_replace("char","",Session::get('message'))}} corresponde a un valor de tipo char (Char) </h5>   
+                             <br>
+                             <h6> El tipo char se utiliza para representar caracteres individuales. Por tanto, el tipo char requiere sólo un byte de memoria. Cada tipo char tiene una interpretación como entero equivalente, de esta forma un char es una clase especial de entero pequeño. En la mayoría de los compiladores el entero tipo char puede tomar valores entre 0 y 255. También se puede utilizar datos unsigned char (con valores de 0 a 255) o datos signed char (con valores de -128 a 127).
+                            </h6>
+                             <br>
+                             <h3 class="alert-heading">¿Qué podemos hacer con este tipo de dato?</h3>
+                              <hr>
+                              <h6>Se puede realizar las siguientes operaciones, por mencionar algunas:</h6>
+                              <ul>
+                                  <li>Declarar en una variable un sólo carácter</li>
+                                  
+                              </ul>
+                              <br>
+                              <h4>Ejemplo en el lenguaje Java</h4>
+                              <h4 class="text-center"><img class="img-fluid" src="https://i.ibb.co/C2rGNdR/example-py.png" width="400"></h4>
+
+                               @elseif(str_contains(Session::get('message'),"float"))
+                            <h5 class="font-weight-bold">El dato {{str_replace("float","",Session::get('message'))}} corresponde a un valor numerico de tipo decimal (float) </h5>   
+                            <br>
+                            <h3 class="alert-heading">¿Qué podemos hacer con este tipo de dato?</h3>
+                             <hr>
+                             <h6>Se puede realizar las siguientes operaciones, por mencionar algunas:</h6>
+                             <ul>
+                                 <li>Suma</li>
+                                 <li>Resta</li>
+                                 <li>Multiplicación</li>
+                                 <li>División</li>
+                                 <li>Potencia</li>
+                                 <li>Raiz cuadrada</li>
+                             </ul>
+                             <br>
+                             <h4>Ejemplo en el lenguaje Python</h4>
+                             <h4 class="text-center"><img class="img-fluid" src="https://i.ibb.co/NSvmYV2/example-py.png" width="400"></h4>
+                          
+                             @elseif(str_contains(Session::get('message'),"char"))
+                             <h5 class="font-weight-bold">El dato {{str_replace("char","",Session::get('message'))}} corresponde a un valor de tipo char (Char) </h5>   
+                             <br>
+                             <h6> El tipo char se utiliza para representar caracteres individuales. Por tanto, el tipo char requiere sólo un byte de memoria. Cada tipo char tiene una interpretación como entero equivalente, de esta forma un char es una clase especial de entero pequeño. En la mayoría de los compiladores el entero tipo char puede tomar valores entre 0 y 255. También se puede utilizar datos unsigned char (con valores de 0 a 255) o datos signed char (con valores de -128 a 127).
+                            </h6>
+                             <br>
+                             <h3 class="alert-heading">¿Qué podemos hacer con este tipo de dato?</h3>
+                              <hr>
+                              <h6>Se puede realizar las siguientes operaciones, por mencionar algunas:</h6>
+                              <ul>
+                                  <li>Declarar en una variable un sólo carácter</li>
+                                  
+                              </ul>
+                              <br>
+                              <h4>Ejemplo en el lenguaje Java</h4>
+                              <h4 class="text-center"><img class="img-fluid" src="https://i.ibb.co/C2rGNdR/example-py.png" width="400"></h4>
+
+                              @else
+                             <h5 class="font-weight-bold">El dato {{str_replace("str","",Session::get('message'))}} corresponde a un valor de tipo cadena (string) </h5>   
+                             <br>
+                             <h6> Contiene secuencias de puntos de código de 16 bits (2 bytes) sin signo que oscilan entre 0 y 65535. Cada punto de código, o código de caracteres, representa un único carácter Unicode. Una cadena puede contener entre 0 y aproximadamente dos mil millones (2 ^ 31) caracteres Unicode.
+                            </h6>
+                             <br>
+                             <h3 class="alert-heading">¿Qué podemos hacer con este tipo de dato?</h3>
+                              <hr>
+                              <h6>Se puede realizar las siguientes operaciones, por mencionar algunas:</h6>
+                              <ul>
+                                  <li>representar y manipular una secuencia de caracteres.</li>
+                                  
+                              </ul>
+                              <br>
+                              <h4>Ejemplo en el lenguaje Python</h4>
+                              <h4 class="text-center"><img class="img-fluid" src="https://i.ibb.co/pf5ZCVt/example-py.png" width="400"></h4>
+                             @endif
+                  
+                    </div>
+                              @endif
+
+                            
                         </form>
 
                     </div>
