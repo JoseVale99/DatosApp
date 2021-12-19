0c8876eb44d2e2c9d875824bde8988eb6d184546
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2021-12-19 21:53:58 +0100

    Use safer pattern() instead of preg_match()

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -22,31 +22,26 @@ class DataController extends Controller
         // $var = str_replace("/","|",$request->dato);
        
         switch (true) {
-            
             case is_null($request->dato):
                 return back()->with('message_error', "¡Por favor ingrese un dato!");
                 break;
-            case preg_match("/^(0|[-1-9][0-9]*)$/", $request->dato):
+            case pattern("^(0|[-1-9][0-9]*)$")->test($request->dato):
                
                 return back()->with('message',"int $request->dato");
                 break;
-            case preg_match("/^([-0-9]+[.]([0-9]+))?$/", $request->dato):
+            case pattern("^([-0-9]+[.]([0-9]+))?$")->test($request->dato):
                 return back()->with('message', "float $request->dato");
                 break;
             case $request->dato == "true" || $request->dato == "false":
                 return back()->with('message', "boolean $request->dato");
                 break;
-            case preg_match("/^([a-z A-Z @ # ~ € & · $ ( )  % = ? { } - + * |])$/", $request->dato):
+            case pattern("^([a-z A-Z @ # ~ € & · $ ( )  % = ? { } - + * |])$")->test($request->dato):
                 return back()->with('message', "char $request->dato");
                 break;
             case is_string($request->dato):
                 return back()->with('message', "str $request->dato");
                 break;
         }
-
-
-
-
 
         return back();
     }
