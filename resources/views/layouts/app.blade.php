7353b8b4f6befc393be4a51a7dc2aa955a19f7bc
Zarate Carreno Jose Valentin <lc19160007@oaxaca.tecnm.mx>
2021-12-30 01:19:11 -0600

    ✅ Adding: nav bar for app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <title>App</title>
 
     <!-- Scripts -->
-    <script src="{{ secure_asset('js/app.js') }}" defer></script>
+    <script src="{{asset('js/app.js') }}" defer></script>
         {{-- secure --}}
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -21,93 +21,45 @@
              type = "image/x-icon">
     <!-- Styles -->
     <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/style.css') }}" rel="stylesheet">
+
 </head>
-<body>
+<body class="color">
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-lg">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    <button type="button" class="btn btn-success" data-toggle="tooltip" data-placement="top" title="Tooltip on top">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="/">PLF</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div class="navbar-nav">
+          
+                <a class="nav-link" href="{{ url('/') }}">
                         Actividad Datos
-                      </button> 
+                     
                 </a>
 
-                <a class="navbar-brand" href="{{ url('/expresiones') }}">
-                    <button type="button" class="btn btn-warning" data-toggle="tooltip" data-placement="top" title="Tooltip on top">
+                <a class="nav-link" href="{{ url('/expresiones') }}">
                         Actividad Expresiones I
-                      </button> 
                 </a>
-                <a class="navbar-brand" href="{{ url('/exp') }}">
-                    <button type="button" class="btn btn-success" data-toggle="tooltip" data-placement="top" title="Tooltip on top">
+                <a class="nav-link " href="{{ url('/exp') }}">
                     Actividad Expresiones II
-                      </button> 
+                     
                 </a>
 
-                <a class="navbar-brand" href="{{ url('/function') }}">
-                    <button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="top" title="Tooltip on top">
+                <a class="nav-link " href="{{ url('/function') }}">
+                    
                    Ecuaciones
-                      </button> 
                 </a>
+                <a class="nav-link " href="{{ url('/Paradigma') }}">
+                    
+                    Paradigmas
+                 </a>
 
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    @if (auth()->user()->is_admin == 0)
-                                        <a class="dropdown-item" href="{{route('home')}}">Your order history</a>
-                                    @endif
-                                    @if (auth()->user()->is_admin == 1)
-                                    <a class="dropdown-item" href="{{route('customers')}}">All Customers</a>
-                                    <a class="dropdown-item" href="{{route('user.order')}}">User Order</a>
-                                @endif
-
-
-
-
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
             </div>
-        </nav>
+        </div>
+      </nav>
+     
 
         <main class="py-4">
             @yield('content')
